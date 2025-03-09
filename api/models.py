86640b3305f74ec0515064a4fa66435986cdd269
Sit-Paing <htun.sitpaing@gmail.com>
2025-03-09 17:29:16 +0700

    Homework 1
    Updated model.

--- api/models.py
+++ api/models.py
@@ -7,6 +7,18 @@ from django.db import models
 class Store(models.Model):
     store_id = models.IntegerField(unique=True)
     store_location = models.CharField(max_length=100)
+    store_created_date = models.DateField(auto_now_add=True)
+    store_updated_date = models.DateField(auto_now=True)
 
     def __str__(self):
         return f"{self.store_id} - {self.store_location}"
+
+
+class Product(models.Model):
+    product_id = models.IntegerField(unique=True)
+    product_name = models.CharField(max_length=100)
+    product_created_date = models.DateField(auto_now_add=True)
+    product_updated_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.product_id} - {self.product_name}"
